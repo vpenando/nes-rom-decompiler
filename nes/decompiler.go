5b8c92257bf2d58cb548d725ee58296fd878bbd9
vpenando <vincent.penando@hotmail.fr>
2020-04-23 00:08:34 +0200

    Fix decompiler

--- nes/decompiler.go
+++ nes/decompiler.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func nextByte(prg *PrgRom) byte {
+	b, err := prg.Next()
+	if err != nil {
+		panic(fmt.Sprintf("Unexpected error: %s", err.Error()))
+	}
+	return b
+}
+
 func Decompile(prg *PrgRom) string {
 	inst, err := prg.Next()
 	if err != nil {
@@ -13,7 +21,7 @@ func Decompile(prg *PrgRom) string {
 	var stringInst string
 	switch inst {
 	case AdcImmediate:
-		stringInst = fmt.Sprintf("ADC %s", ByteToImmediateValue(prg.Next()))
+		stringInst = fmt.Sprintf("ADC %s", ByteToImmediateValue(nextByte(prg)))
 	}
 	return fmt.Sprintf("%s\n%s", stringInst, Decompile(prg))
 }
